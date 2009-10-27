e2f03b61d9b542a6882015aa140e08779d135798
Atif Haider <atif@infinitelybeta.com>
2009-10-27 16:49:15 +0530

    Few more fields added with validators.

--- wtforms-test/forms.py
+++ wtforms-test/forms.py
@@ -12,6 +12,11 @@ email_re = re.compile(
     r'|^"([\001-\010\013\014\016-\037!#-\[\]-\177]|\\[\001-011\013\014\016-\177])*"' # quoted-string
     r')@(?:[A-Z0-9-]+\.)+[A-Z]{2,6}$', re.IGNORECASE)  # domain
 
+url_re = re.compile(
+    r'^(https?://)?' # http:// or https://
+    r'(?:(?:[A-Z0-9-]+\.)+[A-Z]{2,6})' #domain...
+    r'(?:/?|/\S+)$', re.IGNORECASE)
+
 def is_valid_email(form, field):
     """Validates email field
     """
@@ -23,6 +28,15 @@ def is_valid_email(form, field):
         raise ValidationError(u'This field is required')
     return field
 
+def is_valid_url(form, field):
+    """Validates url field
+    """
+    url = field.data
+    if url:
+        if not url_re.match(url):
+            raise ValidationError(u'Enter a valid url')
+    return field
+
 
 class RegistrationForm(Form):
     """A simple class for registration form
@@ -31,9 +45,9 @@ class RegistrationForm(Form):
     password = PasswordField('Password', [validators.length(min=5, max=25)])
     first_name = TextField(u'First Name', validators=[validators.required()])
     last_name  = TextField(u'Last Name', validators=[validators.optional()])
-    # Custom EmailField written
-    email = TextField('Email Address', [is_valid_email])
+    email = TextField('Email Address', [validators.Email(message="Enter a valid email address")])
     avatar = FileField(u'Image File')
+    # Could have used URL validators but doesn't work
+    website = TextField('Website', [is_valid_url])
     birthday = DateTimeField('Your Birthday',  validators=[validators.optional()], format='%m/%d/%y')
-
 
